a0369c8a47a64fa1a06dc5da59577f6b91a33969
Azlarkhon <xasanxojaevazlarxon2004@gmail.com>
2024-06-06 10:28:12 +0500

    first commit

--- routes/cartRoute.go
+++ routes/cartRoute.go
@@ -6,45 +6,12 @@ import (
 )
 
 func CartRoutes(incomingRoutes *gin.Engine) {
-	// @Summary Get cart by ID
-	// @Description Get cart by ID
-	// @Tags Cart
-	// @Param cart_id path string true "Cart ID"
-	// @Success 200 {object} models.Cart
-	// @Router /cart/{cart_id} [get]
+
 	incomingRoutes.GET("/cart/:cart_id", controller.GetCart())
 
-	// @Summary Create a new cart
-	// @Description Create a new cart
-	// @Tags Cart
-	// @Param cart body models.Cart true "Cart"
-	// @Success 200 {object} models.Cart
-	// @Router /cart [post]
 	incomingRoutes.POST("/cart", controller.CreateCart())
-
-	// @Summary Add product to cart
-	// @Description Add product to cart
-	// @Tags Cart
-	// @Param cart_id path string true "Cart ID"
-	// @Param product body models.CartProductRequest true "Product"
-	// @Success 200 {object} models.CartProduct
-	// @Router /cart/{cart_id}/products [post]
 	incomingRoutes.POST("/cart/:cart_id/products", controller.AddProductToCart())
 
-	// @Summary Delete product from cart
-	// @Description Delete product from cart
-	// @Tags Cart
-	// @Param cart_product_id path string true "Cart Product ID"
-	// @Success 204 {string} string "No Content"
-	// @Router /cart_product/{cart_product_id} [delete]
 	incomingRoutes.DELETE("/cart_product/:cart_product_id", controller.DeleteProductFromCart())
-
-	// @Summary Update product quantity in cart
-	// @Description Update product quantity in cart
-	// @Tags Cart
-	// @Param cart_id path string true "Cart ID"
-	// @Param product body models.CartProductRequest true "Product"
-	// @Success 200 {object} models.CartProduct
-	// @Router /cart/products [put]
 	incomingRoutes.PUT("/cart/products", controller.UpdateQuantityOfProduct())
 }
--- routes/productRoute.go
+++ routes/productRoute.go
@@ -6,50 +6,13 @@ import (
 )
 
 func ProductRoutes(incomingRoutes *gin.Engine) {
-	// @Summary Get all products
-	// @Description Get all products
-	// @Tags Products
-	// @Success 200 {array} models.Product
-	// @Router /products [get]
 	incomingRoutes.GET("/products", controller.GetProducts())
-
-	// @Summary Get product by name
-	// @Description Get product by name
-	// @Tags Products
-	// @Param name path string true "Product Name"
-	// @Success 200 {object} models.Product
-	// @Router /products/{name} [get]
 	incomingRoutes.GET("/products/:name", controller.GetProduct())
-
-	// @Summary Filter products
-	// @Description Filter products
-	// @Tags Products
-	// @Success 200 {array} models.Product
-	// @Router /productss [get]
 	incomingRoutes.GET("/productss", controller.FilterProducts())
 
-	// @Summary Add a new product
-	// @Description Add a new product
-	// @Tags Products
-	// @Param product body models.Product true "Product"
-	// @Success 200 {object} models.Product
-	// @Router /products [post]
 	incomingRoutes.POST("/products", controller.AddProduct())
 
-	// @Summary Update product by ID
-	// @Description Update product by ID
-	// @Tags Products
-	// @Param product_id path string true "Product ID"
-	// @Param product body models.Product true "Product"
-	// @Success 200 {object} models.Product
-	// @Router /products/{product_id} [put]
 	incomingRoutes.PUT("/products/:product_id", controller.UpdateProduct())
 
-	// @Summary Delete product by ID
-	// @Description Delete product by ID
-	// @Tags Products
-	// @Param product_id path string true "Product ID"
-	// @Success 204 {string} string "No Content"
-	// @Router /products/{product_id} [delete]
 	incomingRoutes.DELETE("/products/:product_id", controller.DeleteProduct())
 }
